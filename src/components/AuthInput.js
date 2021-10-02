da5a0e60553bcfc07c3edf72e18cc8257eee0dec
jhyoon9705 <jhyoon513@naver.com>
2021-10-02 12:28:16 +0900

    Fix signup screen outline(WIP)

--- src/components/AuthInput.js
+++ src/components/AuthInput.js
@@ -3,22 +3,14 @@ import styled from 'styled-components/native'
 import { useWindowDimensions } from 'react-native'
 
 const Container = styled.View`
-  flex-direction: column;
   width: 100%;
-  margin: 10px 0;
-`
-
-const Label = styled.Text`
-  font-size: 16px;
-  font-weight: 600;
-  margin-bottom: 2px;
-  color: ${({ theme }) => theme.text};
+  margin: 5px 0;
+  padding: 10px 0;
 `
 
 const StyledInput = styled.TextInput`
   width: 100%;
   height: 50px;
-  margin: 5px 0;
   padding: 10px 20px;
   border-radius: 10px;
   background-color: #d4edfd;
@@ -27,16 +19,14 @@ const StyledInput = styled.TextInput`
   margin-bottom: 0px;
 `
 
-const AuthInput = ({ label, placeholder, isPassword, onChangeText }) => {
-  const width = useWindowDimensions().width
+const AuthInput = ({ placeholder, isPassword, onChangeText }) => {
   return (
     <Container>
-      <Label style={{ fontFamily: 'BMHANNAAir_ttf' }}>{label}</Label>
       <StyledInput
-        width={width}
         placeholder={placeholder}
         secureTextEntry={isPassword}
         onChangeText={onChangeText}
+        autoCapitalize="none"
       />
     </Container>
   )
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,14 +1,46 @@
 import React, { useState } from 'react'
 import styled from 'styled-components/native'
-import { Text, Pressable } from 'react-native'
+import {
+  Text,
+  Pressable,
+  View,
+  useWindowDimensions,
+  TextInput,
+} from 'react-native'
 import AuthInput from '../components/AuthInput'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import {
+  Button,
+  WhiteSpace,
+  WingBlank,
+  List,
+  Picker,
+  PickerView,
+  Provider,
+} from '@ant-design/react-native'
 
 const Container = styled.View`
   background-color: ${({ theme }) => theme.background};
   justify-content: center;
   align-items: center;
-  padding: 40px 20px;
+  padding: 30px 20px;
+`
+
+const ErrorText = styled.Text`
+  align-items: flex-start;
+  width: 100%;
+  height: 15px;
+  margin-bottom: 10px;
+  line-height: 15px;
+  color: ${({ theme }) => theme.errorText};
+  font-style: italic;
+`
+
+const ButtonContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  padding: 30px 30px;
 `
 
 const Signup = () => {
@@ -19,85 +51,137 @@ const Signup = () => {
   const [verificationCode, setVerificationCode] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [passwordConfirmError, setPasswordConfirmError] = useState('')
+
+  const _handlePasswordConfirmChange = (passwordConfirm) => {
+    setPasswordConfirm(passwordConfirm)
+    setPasswordConfirmError(
+      password == passwordConfirm ? '' : '! 다시 입력하세요.',
+    )
+  }
 
   return (
-    <KeyboardAwareScrollView extraScrollHeight={20}>
-      <Container>
-        {/* <Text
-          style={{
-            fontSize: 40,
-            color: '#ffffff',
-            fontFamily: 'BMHANNAAir_ttf',
-            marginBottom: 5,
-          }}
-        >
-          회원가입
-        </Text> */}
+    <KeyboardAwareScrollView extraScrollHeight={40}>
+      <Container
+        style={{
+          height: useWindowDimensions().height - 60,
+          width: useWindowDimensions().width,
+        }}
+      >
         <AuthInput
-          label="이름"
           value={name}
           onChangeText={(text) => setName(text)}
-          placeholder="Name"
+          placeholder="이름"
         />
         <AuthInput
-          label="닉네임"
           value={nickName}
           onChangeText={(text) => setNickName(text)}
-          placeholder="Nickname"
+          placeholder="닉네임"
         />
         <AuthInput
-          label="학교명"
           value={university}
           onChangeText={(text) => setUniversity(text)}
-          placeholder="University Name"
+          placeholder="학교명"
         />
-        <AuthInput
-          label="학교 이메일"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          placeholder="Email"
-        />
-        <AuthInput
-          label="인증번호"
-          value={verificationCode}
-          onChangeText={(text) => setVerificationCode(text)}
-          placeholder="Verification code"
-        />
-        <AuthInput
-          label="비밀번호"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          placeholder="Password"
-          isPassword
-        />
-        <AuthInput
-          label="비밀번호 확인"
-          value={passwordConfirm}
-          onChangeText={(text) => setPasswordConfirm(text)}
-          placeholder="Password Confirm"
-          isPassword
-        />
-        <Pressable
-          style={{
-            paddingLeft: 100,
-            paddingRight: 100,
-            paddingTop: 15,
-            paddingBottom: 15,
-            borderRadius: 10,
-            backgroundColor: '#121212',
-            marginBottom: 20,
-          }}
-        >
-          <Text
+        <ButtonContainer>
+          <AuthInput
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            placeholder="학교 이메일"
+          />
+
+          <Button
+            type="primary"
             style={{
-              fontSize: 20,
-              color: 'white',
-              fontFamily: 'BMHANNAAir_ttf',
+              paddingLeft: 13,
+              paddingRight: 13,
+              paddingTop: 5,
+              paddingBottom: 5,
+              borderRadius: 10,
+              marginLeft: 5,
+              backgroundColor: 'white',
             }}
           >
-            회원가입
-          </Text>
-        </Pressable>
+            <Text
+              style={{
+                fontSize: 15,
+                color: 'black',
+                fontFamily: 'BMHANNAAir_ttf',
+              }}
+            >
+              인증{'\n'}요청
+            </Text>
+          </Button>
+        </ButtonContainer>
+        <ButtonContainer>
+          <AuthInput
+            value={verificationCode}
+            onChangeText={(text) => setVerificationCode(text)}
+            placeholder="인증번호"
+          />
+
+          <Button
+            type="primary"
+            style={{
+              paddingLeft: 13,
+              paddingRight: 13,
+              paddingTop: 10,
+              paddingBottom: 10,
+              borderRadius: 10,
+              marginLeft: 5,
+              backgroundColor: 'white',
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 20,
+                color: 'black',
+                fontFamily: 'BMHANNAAir_ttf',
+              }}
+            >
+              인증
+            </Text>
+          </Button>
+        </ButtonContainer>
+
+        <AuthInput
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          placeholder="비밀번호"
+          isPassword
+        />
+        <AuthInput
+          value={passwordConfirm}
+          onChangeText={(text) => _handlePasswordConfirmChange(text)}
+          placeholder="비밀번호 확인"
+          isPassword
+        />
+        <ErrorText>{passwordConfirmError}</ErrorText>
+        <WingBlank>
+          <WhiteSpace />
+          <Button
+            type="primary"
+            style={{
+              paddingLeft: 100,
+              paddingRight: 100,
+              paddingTop: 10,
+              paddingBottom: 10,
+              borderRadius: 10,
+              backgroundColor: '#121212',
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 25,
+                color: 'white',
+                fontFamily: 'BMHANNAAir_ttf',
+              }}
+            >
+              로그인
+            </Text>
+          </Button>
+          <WhiteSpace />
+        </WingBlank>
       </Container>
     </KeyboardAwareScrollView>
   )
