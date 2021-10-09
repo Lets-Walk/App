a2b1b0f1940e481f403584ef8cfe6269f6d17bb4
jhyoon9705 <jhyoon513@naver.com>
2021-10-09 17:27:09 +0900

    Add buttons(inventory etc.) on walking mode screen

--- src/screens/WalkingMode.js
+++ src/screens/WalkingMode.js
@@ -1,13 +1,123 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacit, Pressable } from 'react-native'
+import styled from 'styled-components/native'
+import NaverMapView, {
+  Circle,
+  Marker,
+  Path,
+  Polyline,
+  Polygon,
+} from 'react-native-nmap'
+import GetLocation from 'react-native-get-location'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import MaterialCmIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
-//지도
-//
+// 현재 위치정보 Load -> emulator에서는 google 본사 위치로 고정되어있음
+GetLocation.getCurrentPosition({
+  enableHighAccuracy: true,
+  timeout: 15000,
+})
+  .then((location) => {
+    console.log(location)
+  })
+  .catch((error) => {
+    const { code, message } = error
+    console.warn(code, message)
+  })
+// sample location data
+const P0 = { latitude: 37.564362, longitude: 126.977011 }
+const P1 = { latitude: 37.565051, longitude: 126.978567 }
+const P2 = { latitude: 37.565383, longitude: 126.976292 }
+
+const ButtonContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 10px;
+  justify-content: space-around;
+  padding: 5px;
+  margin: 0px 0px;
+`
+
+const Container = styled.View`
+  flex: 8;
+  align-items: center;
+`
+
+const ButtonText = styled.Text`
+  font-size: 12px;
+  align-items: center;
+
+  font-family: BMHANNAAir_ttf;
+  padding: 3px 0px;
+`
+
 const WalkingMode = ({ navigation }) => {
   return (
-    <View>
-      <Text>워킹모드UI</Text>
-    </View>
+    <>
+      <Container>
+        <NaverMapView
+          style={{ width: '100%', height: '100%' }}
+          showsMyLocationButton={true}
+          center={{ ...P0, zoom: 16 }}
+          onTouch={(e) =>
+            console.warn('onTouch', JSON.stringify(e.nativeEvent))
+          }
+          onCameraChange={(e) =>
+            console.warn('onCameraChange', JSON.stringify(e))
+          }
+          onMapClick={(e) => console.warn('onMapClick', JSON.stringify(e))}
+        >
+          <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')} />
+          <Marker
+            coordinate={P1}
+            pinColor="blue"
+            onClick={() => console.warn('onClick! p1')}
+          />
+          <Marker
+            coordinate={P2}
+            pinColor="red"
+            onClick={() => console.warn('onClick! p2')}
+          />
+        </NaverMapView>
+      </Container>
+      <ButtonContainer>
+        <Pressable onPress={() => console.log('BAG touched')}>
+          <MaterialIcons
+            name="inventory"
+            size={35}
+            color="#4495D0"
+          ></MaterialIcons>
+          <ButtonText>인벤토리</ButtonText>
+        </Pressable>
+        <Pressable onPress={() => console.log('PAPER touched')}>
+          <Ionicons
+            name="newspaper-outline"
+            size={35}
+            color="#4495D0"
+          ></Ionicons>
+          <ButtonText>페이퍼</ButtonText>
+        </Pressable>
+        <Pressable onPress={() => console.log('CHATTING touched')}>
+          <Ionicons
+            name="chatbubble-ellipses-outline"
+            size={35}
+            color="#4495D0"
+          ></Ionicons>
+          <ButtonText>{'\t'}채팅</ButtonText>
+        </Pressable>
+        <Pressable onPress={() => console.log('CLOSE touched')}>
+          <Ionicons
+            name="close-circle-outline"
+            size={35}
+            color="red"
+          ></Ionicons>
+          <ButtonText>{'\t'}종료</ButtonText>
+        </Pressable>
+      </ButtonContainer>
+    </>
   )
 }
 
