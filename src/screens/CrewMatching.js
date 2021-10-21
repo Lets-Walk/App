fb22d2e7f1841ed03f988c66316f7b6fd40e4fcc
jhyoon9705 <jhyoon513@naver.com>
2021-10-21 23:42:37 +0900

    Add code to load campus name on Crew screen

--- src/screens/CrewMatching.js
+++ src/screens/CrewMatching.js
@@ -1,27 +1,73 @@
 import { Button } from '@ant-design/react-native'
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, useWindowDimensions } from 'react-native'
 import ScreenName from '../components/ScreenName'
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { SERVER_URL } from '@env'
 
 const CrewMatching = ({ navigation }) => {
+  const width = useWindowDimensions().width
+  const height = useWindowDimensions().height
+
+  const [campus, setCampus] = useState('')
+
+  // 로그인 된 사용자의 학교명 load
+  useEffect(async () => {
+    let result = null
+    try {
+      const token = await AsyncStorage.getItem('token')
+      const result = await axios.get(SERVER_URL + '/api/auth/me', {
+        headers: {
+          authorization: 'Bearer ' + token,
+          'Content-type': 'application/json',
+          Accept: 'application/json',
+        },
+        timeout: 3000,
+      })
+      setCampus(result.data.user.Campus.name)
+    } catch (err) {
+      console.log(err)
+    }
+  }, [])
+
   return (
     <ScreenName name="워킹크루">
-      <View>
-        <Text>CrewMatching!</Text>
+      <Text>{campus}</Text>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: height * 0.1 + 50,
+          left: (width - width * 0.8) / 2,
+          alignItems: 'center',
+        }}
+      >
         <Button
+          type="primary"
+          style={{
+            backgroundColor: '#4495D0',
+            width: useWindowDimensions().width * 0.8,
+          }}
           onPress={() => {
             navigation.navigate('매칭중')
           }}
         >
           매칭시작
         </Button>
-        <Button
+
+        {/* 임시버튼-> 추후 삭제(워킹모드 진입은 '매칭시작'을 통해서 진행*/}
+        {/* <Button
+          type="primary"
+          style={{
+            backgroundColor: 'black',
+            width: useWindowDimensions().width * 0.5,
+          }}
           onPress={() => {
             navigation.navigate('워킹모드')
           }}
         >
           워킹모드
-        </Button>
+        </Button> */}
       </View>
     </ScreenName>
   )
