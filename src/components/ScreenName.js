c1d393cb12800f145063d773ef1906999af6f78e
jhyoon9705 <jhyoon513@naver.com>
2021-11-09 18:16:00 +0900

    Apply conditional header rendering

--- src/components/ScreenName.js
+++ src/components/ScreenName.js
@@ -8,41 +8,31 @@ const Container = styled.SafeAreaView`
 `
 
 const ScreenName = ({ name, children }) => {
-  return (
-    <Container>
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '8%',
-          marginTop: 5,
-        }}
-      >
-        <Image
-          source={require('../utils/screen_top.png')}
+  if (name !== '워킹크루 매칭') {
+    return (
+      <Container>
+        <View
           style={{
-            aspectRatio: 3353 / 744,
-            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '8%',
+            marginTop: 5,
           }}
-        />
-      </View>
-      {/* <Text
-        style={{
-          fontSize: 40,
-          fontFamily: 'Cafe24Shiningstar',
-          paddingTop: '3%',
-          paddingLeft: '5%',
-          paddingBottom: '3%',
-          textShadowColor: 'gray',
-          textShadowRadius: 2,
-          color: '#1B97E2',
-        }}
-      >
-        {name}
-      </Text> */}
-      {children}
-    </Container>
-  )
+        >
+          <Image
+            source={require('../utils/screen_top.png')}
+            style={{
+              aspectRatio: 3353 / 744,
+              flex: 1,
+            }}
+          />
+        </View>
+        {children}
+      </Container>
+    )
+  } else {
+    return <Container>{children}</Container>
+  }
 }
 
 export default ScreenName
