210c82bb729d9cfa823e94229733983e5d78853c
jjonyo <jonghyuk96@cau.ac.kr>
2021-11-11 16:15:59 +0900

    Add misson svg

---
+++ assets/images/index.js
@@ -0,0 +1,8 @@
+export const Onepair = require('../../assets/images/Onepair.svg')
+export const Twopair = require('../../assets/images/Twopair.svg')
+export const Triple = require('../../assets/images/Triple.svg')
+export const Fourcard = require('../../assets/images/Fourcard.svg')
+export const Flush = require('../../assets/images/Flush.svg')
+export const Straight = require('../../assets/images/Straight.svg')
+export const Fullhouse = require('../../assets/images/Fullhouse.svg')
+export const Misson = require('../../assets/images/Misson.svg')
