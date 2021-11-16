eb83ec2cf198913c2a259ca1244e3edc1cf1bbdd
jjonyo <jonghyuk96@cau.ac.kr>
2021-11-17 00:38:52 +0900

    Add MissionSuccess Modal

--- src/screens/WalkingMode.js
+++ src/screens/WalkingMode.js
@@ -7,7 +7,6 @@ import {
 } from 'react-native'
 import styled from 'styled-components/native'
 import { useFocusEffect } from '@react-navigation/native'
-import Modal from 'react-native-modal'
 import { ActivityIndicator } from '@ant-design/react-native'
 
 import NaverMap from '../components/NaverMap'
@@ -17,6 +16,7 @@ import MissionInfo from '../components/MissionInfo'
 import MissionBanner from '../components/MissionBanner'
 import Banner from '../components/Banner'
 import FinishModal from '../components/FinishModal'
+import MissionSuccess from '../components/MissionSuccess'
 import showToast from '../utils/showToast'
 import { SERVER_URL } from '@env'
 
@@ -27,7 +27,7 @@ const Container = styled.View`
 
 const WalkingMode = ({ route, navigation }) => {
   const { socket, battleRoomId, userInfo, crewId } = route.params
-  const [infoVisible, setInfoVisible] = useState(false)
+  const [infoVisible, setInfoVisible] = useState(false) //미션정보 모달
   const [loading, setLoading] = useState(true)
   const [inventory, setInventory] = useState([])
   const [missionCount, setMissionCount] = useState(null)
@@ -35,6 +35,10 @@ const WalkingMode = ({ route, navigation }) => {
   const [showFinishModal, setFinishModal] = useState(false)
   const [mission, setMission] = useState(null)
   const [crewInfo, setCrewInfo] = useState(route.params.crewInfo)
+  const [successMission, setSuccessMission] = useState({
+    winCampus: null,
+    modalVisible: false,
+  })
 
   useFocusEffect(
     React.useCallback(() => {
@@ -100,9 +104,12 @@ const WalkingMode = ({ route, navigation }) => {
       console.log('inventory초기화')
       setInventory([])
       //맵의 마커를 초기화 하는 작업 필요.
-      Alert.alert(`${campusName}크루가 ${mission}미션을 완료했습니다.`)
       if (isEnd) return //isEnd면 더 이상 진행하지 않고 return
       //미션완료 팝업 후 잠깐 대기한 다음 다음 미션에 대한 준비완료를 알림
+      setSuccessMission({
+        winCampus: campusName,
+        modalVisible: true,
+      })
       setTimeout(() => {
         emitReadyWalkingMode()
       }, 3000)
@@ -163,6 +170,10 @@ const WalkingMode = ({ route, navigation }) => {
           obtainItemEmit={obtainItemEmit}
         />
         <BattleInfo userInfo={userInfo} crewInfo={crewInfo} />
+        <MissionSuccess
+          successMission={successMission}
+          setSuccessMission={setSuccessMission}
+        />
         <MissionTimer show={showTimer} count={missionCount} />
         <MissionBanner missionBannerToggle={missionBannerToggle} />
         <Banner inventory={inventory} />
